eb76476d0fbac037edb0730a88f952b07f3a18d8
Justin Simon <jtsimon@jtsimon.dev>
2024-12-08 16:24:08 -0500

    Add day 2 file

---
+++ AdventOfCode-2024/Day2.cs
@@ -0,0 +1,25 @@
+namespace AdventOfCode_2024;
+
+public class Day2:IAdventOfCodeDayChallenge {
+    public string MenuDescription {get;} = "Red Nose Reports";
+
+    public Day2() {
+
+    }
+
+    public void SolveAll()
+    {
+        SolvePart1();
+        SolvePart2();
+    }
+
+    public void SolvePart1() {
+        throw new NotImplementedException();
+    }
+
+    public void SolvePart2()
+    {
+        throw new NotImplementedException();
+    }
+
+}
