78e1d41bb9637add77f39d6cb910ac13335f2698
Justin Simon <jtsimon@jtsimon.dev>
2024-12-10 23:36:29 -0500

    Get ready for day 5.

---
+++ AdventOfCode-2024/Day5.cs
@@ -0,0 +1,35 @@
+namespace AdventOfCode_2024
+{
+    public class Day5 : IAdventOfCodeDayChallenge
+    {
+        public string MenuDescription {get;} = "Print Queue";
+        private readonly string _inputFilePath;
+
+        public Day5()
+        {
+            _inputFilePath = "./files/day5-input.txt";
+        }
+
+        public void SolveAll()
+        {
+            SolvePart1();
+            //SolvePart2();
+        }
+
+        public void SolvePart1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SolvePart2()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoadData()
+        {
+
+
+        }
+    }
+}
--- AdventOfCode-2024/Program.cs
+++ AdventOfCode-2024/Program.cs
@@ -10,6 +10,7 @@
                 new Day2(),
                 new Day3(),
                 new Day4(),
+                new Day5(),
             };
 
             while (true)
