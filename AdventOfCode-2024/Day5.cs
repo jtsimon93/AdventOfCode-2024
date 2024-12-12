543db2aea3f83bbd6c9de9cd1c479e2f8052e63f
Justin Simon <jtsimon@jtsimon.dev>
2024-12-11 19:22:42 -0500

    Update vars

--- AdventOfCode-2024/Day5.cs
+++ AdventOfCode-2024/Day5.cs
@@ -1,19 +1,17 @@
-using System.Runtime.InteropServices.Marshalling;
-
 namespace AdventOfCode_2024
 {
     public class Day5 : IAdventOfCodeDayChallenge
     {
-        public string MenuDescription {get;} = "Print Queue";
+        public string MenuDescription { get; } = "Print Queue";
         private readonly string _inputFilePath;
         private Dictionary<int, List<int>> _updateRules;
-        private List<int> _pageNumbers;
+        private List<List<int>> _updateBatches;
 
         public Day5()
         {
             _inputFilePath = "./files/day5-input.txt";
-            _updateRules = new Dictionary<int,List<int>>();
-            _pageNumbers = new List<int>();
+            _updateRules = new Dictionary<int, List<int>>();
+            _updateBatches = new List<List<int>>();
             LoadData();
         }
 
@@ -39,47 +37,53 @@ namespace AdventOfCode_2024
             {
                 string? currentLine;
                 bool emptyLineEncountered = false;
-                while((currentLine = reader.ReadLine()) != null)
+                while ((currentLine = reader.ReadLine()) != null)
                 {
-                    if(currentLine == string.Empty) {
+                    if (currentLine == string.Empty)
+                    {
                         emptyLineEncountered = true;
                         continue;
                     }
-                    if(!emptyLineEncountered)
+                    if (!emptyLineEncountered)
                     {
                         string[] nums = currentLine.Split("|");
 
                         var num1Parse = int.TryParse(nums[0], out int num1);
                         var num2Parse = int.TryParse(nums[1], out int num2);
 
-                        if(!num1Parse || !num2Parse)
+                        if (!num1Parse || !num2Parse)
                         {
                             Console.WriteLine($"Invalid input on line: {currentLine}");
                             continue;
                         }
 
-                        if(!_updateRules.ContainsKey(num1))
+                        if (!_updateRules.ContainsKey(num1))
                         {
                             _updateRules[num1] = new List<int>();
                         }
 
                         _updateRules[num1].Add(num2);
                     }
-                    else {
+                    else
+                    {
                         string[] nums = currentLine.Split(",");
 
-                        foreach(var num in nums)
+                        List<int> singleUpdateBatch = new List<int>();
+
+                        foreach (var num in nums)
                         {
                             var parseNum = int.TryParse(num, out int parsedNum);
 
-                            if(!parseNum)
+                            if (!parseNum)
                             {
                                 Console.WriteLine($"Invalid input on line: {currentLine}");
                                 continue;
                             }
 
-                            _pageNumbers.Add(parsedNum);
+                            singleUpdateBatch.Add(parsedNum);
                         }
+
+                        _updateBatches.Add(singleUpdateBatch);
                     }
                 }
             }
